fbc1c79f0e65c69a6304ee69809f25f1f886f2eb
Ganza Ntambuko <ganzantambuko@gmail.com>
2021-04-27 12:03:55 +0200

    hh

--- myquiz/urls.py
+++ myquiz/urls.py
@@ -6,9 +6,8 @@ from quizapi import views
 
 urlpatterns = [
 
-	
+	url(r'^base/$', index_views.base),
 	url(r'^$', index_views.index),
-	url(r'^$', index_views.base),
 	url(r'^login/$', index_views.login),
 	url(r'^questions/', include('quiz.urls')),
 	url(r'^quizapi/', views.QuizApiList.as_view()),
