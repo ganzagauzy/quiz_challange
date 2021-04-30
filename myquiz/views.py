46d9cd3dd4bc59b28676c2dbd93f8481eafde5be
Ganza Ntambuko <ganzantambuko@gmail.com>
2021-04-30 09:30:01 +0200

    gjk

--- myquiz/views.py
+++ myquiz/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 
 
-def base(request):
-	return render(request, 'base.html')
+
 
 def index(request):
 	return render(request, 'index.html')
+def home(request):
+	return render(request, 'home.html')	
 
 def questions(request):
 	return render(request, 'quiz.html')
