b3d431897f6c431759d7349b7ac8a259378cdfa0
Ganza Ntambuko <ganzantambuko@gmail.com>
2021-04-30 11:38:23 +0200

    1fnl

--- static/js/quiz.js
+++ static/js/quiz.js
@@ -36,7 +36,7 @@ function submitAnswers(answers){
 	
 	var results = document.getElementById('results');
 	results.innerHTML = "<h3>You scored <span>" + score + "</span> out of <span>" + total + "</span></h3>"
-	alert( "You scored " + score + " out of " + total);
+	alert( "You scored " + login + score + " out of " + total);
 
 	return false;
 }
